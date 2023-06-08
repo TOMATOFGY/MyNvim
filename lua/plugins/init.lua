e83a21e153415c539045b8b6e22d23f82fb04100
TOMATOFAQ <tomatofaq@gmail.com>
2023-06-08 20:17:06 +0800

    add extension notice

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -301,7 +301,24 @@ local default_plugins = {
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
   }
-  }
+  },
+  -- lazy.nvim
+{
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  opts = {
+    -- add any options here
+  },
+  dependencies = {
+    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+    "MunifTanjim/nui.nvim",
+    -- OPTIONAL:
+    --   `nvim-notify` is only needed, if you want to use the notification view.
+    --   If not available, we use `mini` as the fallback
+    "rcarriga/nvim-notify",
+    }
+}
+
 }
 
 local config = require("core.utils").load_config()
