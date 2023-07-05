663399a5ec172db278302038136c6044e9db6265
TOMATOFAQ <tomatofaq@gmail.com>
2023-07-05 13:53:32 +0800

    添加了 reach 模块,能够明确自己将会跳转到什么 buffer

--- config.lua
+++ config.lua
@@ -6,6 +6,16 @@
 
 lvim.plugins = {
   {
+    'toppair/reach.nvim',
+    config = function()
+      -- default config
+      reach = require('reach')
+      reach.setup({
+        notifications = true
+      })
+    end
+  },
+  {
     'norcalli/nvim-colorizer.lua',
     config = function()
       require 'colorizer'.setup({
@@ -354,3 +364,77 @@ lvim.builtin.terminal.direction = 'horizontal'
 
 -- alias jk as <ESC> in input mode
 lvim.keys.insert_mode["jk"] = "<ESC>"
+
+
+-- default
+local mark_options = {
+  filter = function(mark)
+    return mark:match('[a-zA-Z]') -- return true to disable
+  end,
+  -- A map of action to key that should be used to invoke it
+  actions = {
+    split = '-',
+    vertsplit = '|',
+    tabsplit = ']',
+    delete = '<Space>',
+  },
+}
+
+lvim.keys.normal_mode["'"] = function()
+  require('reach').marks(mark_options)
+end
+
+-- default
+local options = {
+  handle = 'auto',        -- 'bufnr' or 'dynamic' or 'auto'
+  show_icons = true,
+  show_current = false,   -- Include current buffer in the list
+  show_modified = true,   -- Show buffer modified indicator
+  modified_icon = '⬤',  -- Character to use as modified indicator
+  grayout_current = true, -- Wheter to gray out current buffer entry
+  force_delete = {},      -- List of filetypes / buftypes to use
+  -- 'bdelete!' on, e.g. { 'terminal' }
+  filter = nil,           -- Function taking bufnr as parameter,
+  -- returning true or false
+  sort = function(n1, n2)
+    -- TODO 编写排序的逻辑
+    return true
+  end,                  -- Comparator function (bufnr, bufnr) -> bool
+  terminal_char = '\\', -- Character to use for terminal buffer handles
+  -- when options.handle is 'dynamic'
+  grayout = true,       -- Gray out non matching entries
+
+  -- A list of characters to use as handles when options.handle == 'auto'
+  auto_handles = require('reach.buffers.constant').auto_handles,
+  auto_exclude_handles = {}, -- A list of characters not to use as handles when
+  -- options.handle == 'auto', e.g. { '8', '9', 'j', 'k' }
+  previous = {
+    enable = true,   -- Mark last used buffers with specified chars and colors
+    depth = 2,       -- Maximum number of buffers to mark
+    chars = { '•' }, -- Characters to use as markers,
+    -- last one is used when depth > #chars
+    groups = {       -- Highlight groups for markers,
+      'String',      -- last one is used when depth > #groups
+      'Comment',
+    },
+  },
+  -- A map of action to key that should be used to invoke it
+  actions = {
+    split = '-',
+    vertsplit = '|',
+    tabsplit = ']',
+    delete = '<Space>',
+    priority = '=',
+  },
+}
+
+
+lvim.builtin.which_key.mappings["b"] = {
+  function()
+    require('reach').buffers(options)
+  end,
+  "open buffers"
+}
+
+
+-- SNIPPET
